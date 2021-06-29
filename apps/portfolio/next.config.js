646bd7c581dc4358132293f96d1226c4dc155be7
Marzooq <43886014+Marzooq13579@users.noreply.github.com>
2021-06-29 11:05:34 +0530

    Update next.config.js

--- apps/portfolio/next.config.js
+++ apps/portfolio/next.config.js
@@ -2,6 +2,11 @@
 const withNx = require('@nrwl/next/plugins/with-nx');
 
 module.exports = withNx({
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   nx: {
     // Set this to false if you do not want to use SVGR
     // See: https://github.com/gregberge/svgr
